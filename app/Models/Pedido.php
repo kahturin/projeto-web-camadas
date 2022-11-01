5f740634e43cb3bdb7edecbea48aaf575aa61e45
moasa <moasaraujo@gmail.com>
2022-11-01 20:37:04 -0300

    model pedidos

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['produto_nome','produto_desc','produto_preco','produto_desconto','produto_ativo'];
+
+    protected $primaryKey = 'pkproduto';
+
+    protected $table = 'produto';
+
+    public $incrementing = true;
+
+
+
 }
